b74bb292b05375a5a919f2dee6a76665a1d8c58d
Dzmitry_Antsybor <Dzmitry.Antsibor.Consultant@consumer.org>
2016-10-13 18:16:45 +0300

    setup gradle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
+import org.apache.commons.lang3.StringUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Parent Module ...");
+        System.out.println(StringUtils.trim("Parent Module ..."));
     }
 }
